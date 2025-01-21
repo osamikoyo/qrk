5a8dc51b6d67e1bae96d5c3ea14b8264344dc705
osamikoyo <v.pylayev@mail.ru>
2025-01-21 23:25:54 +0500

    loger

--- internal/transtort/handler/handler.go
+++ internal/transtort/handler/handler.go
@@ -1,1 +1,17 @@
 package handler
+
+import (
+	"net/http"
+
+	"github.com/osamikoyo/qrk/pkg/loger"
+)
+
+type handler func (w http.ResponseWriter, r *http.Request) error 
+
+func errorRoute(h handler) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if err := h(w, r);err != nil{
+			loger.New().Error().Err(err)
+		}
+	}
+}
