6d133c3dee81485243302943a268a49059500aec
osamikoyo <v.pylayev@mail.ru>
2025-01-22 12:44:56 +0500

    add handler

--- internal/transtort/handler/handler.go
+++ internal/transtort/handler/handler.go
@@ -1,10 +1,18 @@
 package handler
 
 import (
+	"image/png"
 	"net/http"
 
+	"github.com/osamikoyo/qrk/internal/utils"
 	"github.com/osamikoyo/qrk/pkg/loger"
 )
+
+type Handler struct{}
+
+func New() *Handler {
+	return new(Handler)
+}
 
 type handler func (w http.ResponseWriter, r *http.Request) error 
 
@@ -14,4 +22,21 @@ func errorRoute(h handler) http.HandlerFunc {
 			loger.New().Error().Err(err)
 		}
 	}
+}
+
+func (h *Handler) getQR(w http.ResponseWriter, r *http.Request) error {
+	if r.Method != http.MethodGet{
+		r.Response.StatusCode = http.StatusMethodNotAllowed
+		http.Error(w, "method not allowed", http.StatusMethodNotAllowed)
+	}
+	
+	title := r.URL.Query().Get("link")
+	code := utils.GetQR(title, &w)
+
+	return png.Encode(w, code)
+}
+
+func RegisterRoute(mux *http.ServeMux){
+	handler := New()
+	mux.Handle("/qr", errorRoute(handler.getQR))
 }
